13a2303fb6be74cfc046e4db82ef02ec354fd66e
ANKAN DALUI <143159255+ankandalui@users.noreply.github.com>
2024-10-07 12:32:49 +0530

    Update menuItems.js

--- components/Layout/menuItems.js
+++ components/Layout/menuItems.js
@@ -11,36 +11,38 @@ export const menuItems = [
 		title: "Services",
 		path: "services",
 	},
-	{
-		title: "Pages",
-		path: "#",
-		submenu: [
-			{
-				title: "Faq",
-				path: "faq",
-			},
-			{
-				title: "Team",
-				path: "team",
-			},
-			{
-				title: "Portfolio",
-				path: "projects",
-			},
-			{
-				title: "Pricing",
-				path: "pricing",
-			},
-			{
-				title: "404",
-				path: "404",
-			},
-		],
-	},
-	{
-		title: "News",
-		path: "news",
-	},
+	// {
+	// 	title: "Pages",
+	// 	path: "#",
+	// 	submenu: [
+	// 		{
+	// 			title: "Faq",
+	// 			path: "faq",
+	// 		},
+	// 		{
+	// 			title: "Team",
+	// 			path: "team",
+	// 		},
+	// 		{
+	// 			title: "Portfolio",
+	// 			path: "projects",
+	// 		},
+	// 		{
+	// 			title: "Pricing",
+	// 			path: "pricing",
+	// 		},
+	// 		{
+	// 			title: "404",
+	// 			path: "404",
+	// 		},
+	// 	],
+	// },
+	// {
+	// 	title: "News",
+	// 	path: "news",
+	// },
+	{title:"Policy",
+	path:"/policy"},
 	{
 		title: "Contact",
 		path: "contact",
