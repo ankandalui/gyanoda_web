095971c1eecdb4292b614c6f5c213e0a51c1eee0
ANKAN DALUI <143159255+ankandalui@users.noreply.github.com>
2024-10-07 12:25:07 +0530

    Update menuItems.js

--- components/Layout/menuItems.js
+++ components/Layout/menuItems.js
@@ -1,22 +1,22 @@
 export const menuItems = [
-	{
-		title: "Demo",
-		path: "#",
-		submenu: [
-			{
-				title: "Home 1",
-				path: "/",
-			},
-			{
-				title: "Home 2",
-				path: "index-two",
-			},
-			{
-				title: "Home 3",
-				path: "index-three",
-			},
-		],
-	},
+	// {
+	// 	title: "Demo",
+	// 	path: "#",
+	// 	submenu: [
+	// 		{
+	// 			title: "Home 1",
+	// 			path: "/",
+	// 		},
+	// 		{
+	// 			title: "Home 2",
+	// 			path: "index-two",
+	// 		},
+	// 		{
+	// 			title: "Home 3",
+	// 			path: "index-three",
+	// 		},
+	// 	],
+	// },
 	{
 		title: "About",
 		path: "about",
