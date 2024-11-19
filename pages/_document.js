aa0d7fc00c744ee8851e75294110442d3917092f
sandipan-das-sd <sd901656@gmail.com>
2024-11-20 00:40:11 +0530

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,30 @@
 import { Head, Html, Main, NextScript } from "next/document";
-const GTM_ID = 'GTM-5P6K7GDM'; 
+const GTM_ID = 'GTM-5P6K7GDM';
 export default function Document() {
 	return (
-		
+
 		<Html>
 			<Head>
-			<script
-            dangerouslySetInnerHTML={{
-              __html: `
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1852241678410881"
+					crossOrigin="anonymous"
+				/>
+				<meta
+					name="google-adsense-account"
+					content="ca-pub-1852241678410881"
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','${GTM_ID}');
               `,
-            }}
-          />
+					}}
+				/>
 				<link href="/fonts/GeneralSans-Bold.eot" rel="stylesheet" />
 				<link href="/fonts/GeneralSans-Bold.woff" rel="stylesheet" />
 				<link href="/fonts/GeneralSans-Bold.woff2" rel="stylesheet" />
@@ -37,14 +46,14 @@ export default function Document() {
 				<link href="/fonts/xmoze.woff2" rel="stylesheet" />
 			</Head>
 			<body>
-			<noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
+				<noscript>
+					<iframe
+						src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+						height="0"
+						width="0"
+						style={{ display: 'none', visibility: 'hidden' }}
+					/>
+				</noscript>
 				<Main />
 				<NextScript />
 			</body>
