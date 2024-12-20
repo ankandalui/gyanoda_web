a7db000d457154584c447fdfc3992dc08c77dcfc
sandipan-das-sd <sd901656@gmail.com>
2024-12-20 18:19:18 +0530

    google tag added

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,160 @@
+// import { Head, Html, Main, NextScript } from "next/document";
+// const GTM_ID = 'GTM-5P6K7GDM';
+// const GA_ID = 'G-VMHLXBPR06';
+// export default function Document() {
+// 	return (
+
+// 		<Html>
+// 			<Head>
+
+// 				{/* Usercentrics Consent Management Platform */}
+// 				<script
+// 					id="usercentrics-cmp"
+// 					src="https://app.usercentrics.eu/browser-ui/latest/loader.js"
+// 					data-settings-id="njLuVAxlVYQd4y"
+// 					async
+// 				/>
+
+// 				{/* Google Tag Manager - Load after consent */}
+// 				<script
+// 					dangerouslySetInnerHTML={{
+// 						__html: `
+//               // Initialize dataLayer
+//               window.dataLayer = window.dataLayer || [];
+              
+//               // Wait for Usercentrics to be ready
+//               window.addEventListener('ucEvent', function(e) {
+//                 if (e.detail.event === 'consent_status') {
+//                   const services = e.detail.data;
+                  
+//                   // Check if marketing consent is given
+//                   if (services.marketing === true) {
+//                     // Initialize GTM
+//                     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//                     })(window,document,'script','dataLayer','${GTM_ID}');
+
+//                     // Initialize Google Analytics
+//                     window.dataLayer = window.dataLayer || [];
+//                     function gtag(){dataLayer.push(arguments);}
+//                     gtag('js', new Date());
+//                     gtag('config', '${GA_ID}', {
+//                       'consent': 'granted'
+//                     });
+//                   }
+//                 }
+//               });
+//             `,
+// 					}}
+// 				/>
+// 				{/* Google AdSense */}
+// 				<script
+// 					async
+// 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7117601803338811"
+// 					crossOrigin="anonymous"
+// 				/>
+// 				<meta
+// 					name="google-adsense-account"
+// 					content="ca-pub-7117601803338811"
+// 				/>
+
+// 				{/* Google Tag Manager */}
+// 				{/* <script
+// 					dangerouslySetInnerHTML={{
+// 						__html: `
+//                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//                 })(window,document,'script','dataLayer','${GTM_ID}');
+//               `,
+// 					}}
+// 				/> */}
+// 				{/* Google Analytics */}
+// 				<script
+// 					async
+// 					src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+// 				/>
+// 				<script
+// 					dangerouslySetInnerHTML={{
+// 						__html: `
+//               window.dataLayer = window.dataLayer || [];
+//               function gtag(){dataLayer.push(arguments);}
+//               gtag('js', new Date());
+//               gtag('config', '${GA_ID}');
+//             `,
+// 					}}
+// 				/>
+// 				<link href="/fonts/GeneralSans-Bold.eot" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Bold.woff" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Bold.woff2" rel="stylesheet" />
+
+// 				<link href="/fonts/GeneralSans-Medium.eot" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Medium.woff" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Medium.woff2" rel="stylesheet" />
+
+// 				<link href="/fonts/GeneralSans-Regular.eot" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Regular.woff" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Regular.woff2" rel="stylesheet" />
+
+// 				<link href="/fonts/GeneralSans-Semibold.eot" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Semibold.woff" rel="stylesheet" />
+// 				<link href="/fonts/GeneralSans-Semibold.woff2" rel="stylesheet" />
+
+// 				<link href="/fonts/xmoze.eot" rel="stylesheet" />
+// 				<link href="/fonts/xmoze.woff" rel="stylesheet" />
+// 				<link href="/fonts/xmoze.woff2" rel="stylesheet" />
+// 				<meta name="google-adsense-account" content="ca-pub-1852241678410881"></meta>
+// 			</Head>
+// 			<body>
+// 				<noscript>
+// 					<iframe
+// 						src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+// 						height="0"
+// 						width="0"
+// 						style={{ display: 'none', visibility: 'hidden' }}
+// 					/>
+// 				</noscript>
+// 				<Main />
+// 				<NextScript />
+// 			</body>
+// 		</Html>
+// 	);
+// }
 import { Head, Html, Main, NextScript } from "next/document";
 const GTM_ID = 'GTM-5P6K7GDM';
 const GA_ID = 'G-VMHLXBPR06';
+
 export default function Document() {
 	return (
-
 		<Html>
 			<Head>
+				{/* Google tag (gtag.js) */}
+				<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11507505504"></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'AW-11507505504');
+						`,
+					}}
+				/>
+
+				{/* Event snippet for Purchase conversion page */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							gtag('event', 'conversion', {
+								'send_to': 'AW-11507505504/QQE2CLL4gvoZEOCymu8q',
+								'transaction_id': ''
+							});
+						`,
+					}}
+				/>
 
 				{/* Usercentrics Consent Management Platform */}
 				<script
@@ -49,6 +198,7 @@ export default function Document() {
             `,
 					}}
 				/>
+
 				{/* Google AdSense */}
 				<script
 					async
@@ -72,6 +222,7 @@ export default function Document() {
               `,
 					}}
 				/> */}
+
 				{/* Google Analytics */}
 				<script
 					async
@@ -87,6 +238,7 @@ export default function Document() {
             `,
 					}}
 				/>
+
 				<link href="/fonts/GeneralSans-Bold.eot" rel="stylesheet" />
 				<link href="/fonts/GeneralSans-Bold.woff" rel="stylesheet" />
 				<link href="/fonts/GeneralSans-Bold.woff2" rel="stylesheet" />
